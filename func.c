2177180e2d5b0a06086cfe89bab9c18cb041c4e1
fedelauberer <fedegarcia@itba.edu.ar>
2021-07-06 13:07:50 -0300

    funciones

---
+++ func.c
@@ -0,0 +1,35 @@
+#include "func.h"
+
+#define WRONG_ARG_COUNT 1
+#define FILE_NOT_FOUND 2
+#define OK 0
+
+unsigned int checkArgs(int argc, char ** argv, FILE * archivo){
+	if (argc != 2) // Solo debe haber un argumento (sumado al nombre del archivo a ejecutar)
+		return WRONG_ARG_COUNT; // Si hay mas de un argumento devuelvo codigo de error 1
+    archivo = fopen(argv[1], “rt”); // "Abro" archivo si existe, si no existe devuelve NULL
+    if (archivo == NULL) // Si no existe el archivo devuelvo codigo de error 2
+		return FILE_NOT_FOUND;
+	return OK;
+}
+
+void AddQ1Line(char * year, char * films, char * series, FILE * stream){
+    fputs(year, stream);
+    fputc(';', stream);
+    fputs(films, stream);
+    fputc(';', stream);
+    fputs(series, stream);
+    fputc('\n', stream);
+    return;
+}
+
+void AddQ2Line(char * year, char ** genres, , stream){
+    return;
+}
+        
+void AddQ3Line(char * str, stream){
+    fputs(str, stream);
+    fputc('\n', stream);
+    return;
+}
+
