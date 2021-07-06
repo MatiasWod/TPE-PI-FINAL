ddbc453d96a95a2618986a063958eab7c30a9fdb
fedelauberer <fedegarcia@itba.edu.ar>
2021-07-06 13:12:19 -0300

    funciones.h

---
+++ func.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+unsigned int checkArgs(int argc, char ** argv, FILE * archivo);
+
+AddQ1Line(year, getFilms(list), getSeries(list), stream);
+
+AddQ2Line(year, getGenre(list, cantFilms), cantFilms, stream);
+        
+AddQ3Line(getMostVoted(list), stream);
