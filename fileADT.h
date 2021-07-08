9349129e1416b8ee02fabc9cba6e5a18872005cb
AndresCarro <acarro@itba.edu.ar>
2021-07-08 10:28:19 -0300

    pongo un puntero a nodo

--- fileADT.h
+++ fileADT.h
@@ -3,16 +3,19 @@
 #include <stdio.h>
 //El file se recorrera una unica vez entonces no hace falta un iterador de lista
 //ni nada ya que se va a liberar y guardar linea por linea a medida que se va 
-//recorriendo el archivo
+//recorriendo el archivo. Los generos que tiene el file se deben guardar en una 
+//lista.
 
 typedef struct line* LineADT;
+
+typedef struct node * TList;
 
 /*Creo un nuevo adt a partir de un puntero a file que se le pasa a la funcion.*/
 LineADT newLine(void);
 
 /*Llamo a feof para ver si llegue al final del archivo.
 Devuelve algo distinto de 0 si llegue al final del archivo y 0 sino*/
-int hasNextLine(LineADT line,FILE*file);
+int hasNextLine(FILE*file);
 
 /*Recorro la linea del file y lo voy almacenando en el ADT
 Retorna 0 si no hay memoria,1 si la linea no es una serie y tampoco una pelicula*/
