67f8620d5e28b481504e301705548566f215afdf
fedelauberer <fedegarcia@itba.edu.ar>
2021-07-08 19:53:00 -0300

    Pequenios cambios

--- queryADT.h
+++ queryADT.h
@@ -1,14 +1,16 @@
 #ifndef __query__h
 #define __query__h
 #include "fileADT.h"
-
+#include <stdlib.h>
 typedef struct queryCDT *queryADT;
 
-static int compare(unsigned int year1,unsigned int year2){
-        return year1-year2;
-}
+// static int compare(unsigned int year1,unsigned int year2){
+//         return ((int)year1-(int)year2);
+// }
 
 queryADT newQuery(void);//Crea la query vacia
+
+void printList(queryADT query);
 
 unsigned int add(queryADT query,LineADT data);//Agrega nueva pelicua/serie con sus datos y devuelve 1 si se pudo agregar y 0 si no pudo.
 
