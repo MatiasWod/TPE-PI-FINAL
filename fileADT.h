1391c1ccb45be5927a12d69e19f4c124e260bed4
AndresCarro <acarro@itba.edu.ar>
2021-07-09 14:18:58 -0300

    cambios en los comentarios

--- fileADT.h
+++ fileADT.h
@@ -4,9 +4,6 @@
 
 #define MOVIE 1
 #define TV_SERIES 2
-//El file se recorrera una unica vez entonces no hace falta un iterador de lista
-//ni nada ya que se va a liberar y guardar linea por linea a medida que se va 
-//recorriendo el archivo
 
 typedef struct line* LineADT;
 
@@ -17,72 +14,66 @@ typedef struct node{
 
 typedef struct node * TList;
 
-/*---FUNCIONES A BORRAR---*/
-//SON PARA TESTEO
-
-// void printList(LineADT line);
-
-void printListRec(TList list);
-
-/*---ACA TERMINA EL DELIMITADOR DE FUNCIONES A BORRAR---*/
-
 /*
-Creo un nuevo adt a partir de un puntero a file que se le pasa a la funcion.
+Creo un nuevo adt. Devuelve un puntero al struct y NULL si no pudo
+alocar memoria para el adt.
 */
 LineADT newLine(void);
 
 /*
 Llamo a feof para ver si llegue al final del archivo.
-Devuelve algo distinto de 0 si llegue al final del archivo y 0 sino
+Devuelve algo distinto de 0 si tiene una linea siguiente y 0 si se llego
+al final del archivo.
 */
 int hasNextLine(FILE*file);
 
 /*
-Recorro la linea del file y lo voy almacenando en el ADT
-Retorna 0 si no hay memoria,1 si la linea no es una serie y tampoco una pelicula
+Recorro la linea del file y lo voy almacenando en el ADT. Retorna 3 si 
+no hay memoria, retorna 1 si la linea no es una serie ni una pelicula y
+retorna 0 si se se añadio correctamente al ADT. 
 */
 int nextLine(LineADT line,FILE*file);
 
 /*
-Devuelve el titleType del line en el que esta parado
+Devuelve el titleType del line en el que esta parado.
 */
 char getTitleType(LineADT line);
 
 /*
 Devuelve una copia del string del primaryTitle
-Retorna el puntero a la copia o NULL si no se pudo alocar memoria correctamente
+Retorna el puntero a la copia o NULL si no se pudo alocar memoria correctamente.
 */
 char *getPrimaryTitle(LineADT line);
 
 /*
-Devuelve el startYear (en unsigned int) de la linea del file
+Devuelve el startYear (en unsigned int) de la linea del file.
 */
 unsigned int getStartYear(LineADT line);
 
 /*
-Devuelve una copia del primer nodo de la lista o NULL si no se pudo
-alocar memoria correctamente
+Devuelve la matriz de generos de la linea del file.
 */
-// TList getFirstGenre (LineADT line);
-char **getFirstGenre (LineADT line);
+char **getGenres (LineADT line);
+
 /*
-Devuelve un string del averageRating de la linea del file
-Retorna el puntero a la copia o NULL si no se pudo alocar memoria correctamente
+Devuelve un string del averageRating de la linea del file.
+Retorna el puntero a la copia o NULL si no se pudo alocar memoria correctamente.
 */
 char * getAverageRating(LineADT line);
 
 /*
-Devuelve el numVotes(en unsigned int) de la linea del file
+Devuelve el numVotes(en unsigned int) de la linea del file.
 */
 unsigned int getNumVotes(LineADT line);
 
 /*
-Libero la linea en la que estoy parado
+Libera los campos del struct que tienen alocada memoria en el heap
+y les asigna NULL a cada puntero para la siguiente iteracion del LineADT.
 */
 void freeLine(LineADT line);
 
 /*
-Libero el struct
+Libero el struct, sin liberar los campos.
 */
 void freeLineADT(LineADT line);
 
